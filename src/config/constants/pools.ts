c26d5f974e9e07190b619f5a60c8f76a0db85e23
Dung Hoang <itmanvn07@gmail.com>
2021-09-14 01:47:03 +0700

    first commit

---
+++ src/config/constants/pools.ts
@@ -0,0 +1,21 @@
+import tokens from './tokens'
+import { PoolConfig, PoolCategory } from './types'
+
+const pools: PoolConfig[] = [
+  {
+    sousId: 0,
+    stakingToken: tokens.cake,
+    earningToken: tokens.soli,
+    contractAddress: {
+      97: '0x963DdEb0C5d3F9382d98972Af48590297cCdB74d',
+      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+  }
+]
+
+export default pools
